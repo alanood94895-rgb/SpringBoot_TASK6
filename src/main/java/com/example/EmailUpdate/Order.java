7eb038291b0a967541fea807b41f8e838fcb8169
CompanyProfile <alanood94895@gmail.com>
2026-06-07 11:29:41 +0400

    Add new class name Order

---
+++ src/main/java/com/example/EmailUpdate/Order.java
@@ -0,0 +1,41 @@
+package com.example.EmailUpdate;
+
+public class Order {
+    private int orderId;
+    private String shippingAddress;
+    private String orderStatus;
+
+    // Constructor
+    public Order(int orderId, String shippingAddress, String orderStatus) {
+        this.orderId = orderId;
+        this.shippingAddress = shippingAddress;
+        this.orderStatus = orderStatus;
+    }
+
+    // Getters
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    // Setters
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
+}
