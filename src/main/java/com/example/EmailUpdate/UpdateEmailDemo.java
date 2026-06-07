94275bf82350ef2b3fb8ef37f63434e8422d1ce0
CompanyProfile <alanood94895@gmail.com>
2026-06-07 10:51:09 +0400

    Add New email for update

--- src/main/java/com/example/EmailUpdate/UpdateEmailDemo.java
+++ src/main/java/com/example/EmailUpdate/UpdateEmailDemo.java
@@ -4,6 +4,7 @@ public class UpdateEmailDemo {
     public static void main(String[] args) {
 
         String oldEmail = "ahmed@example.com";
+        String newEmail = "ali@example.com";
 
 
     }
