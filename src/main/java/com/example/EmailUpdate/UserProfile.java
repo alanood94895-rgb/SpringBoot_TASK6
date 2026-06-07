6ef744f76c74663bea7328f14b7f25d2f3cf70f9
CompanyProfile <alanood94895@gmail.com>
2026-06-07 11:19:31 +0400

    Add new class name UserProfile

--- src/main/java/com/example/EmailUpdate/UserProfile.java
+++ src/main/java/com/example/EmailUpdate/UserProfile.java
@@ -1,4 +1,34 @@
 package com.example.EmailUpdate;
 
 public class UserProfile {
+    private int userId;
+    private String phoneNumber;
+    private String address;
+
+    public UserProfile(int userId, String phoneNumber, String address) {
+        this.userId = userId;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
+
